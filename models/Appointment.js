78c2269aa4c54bdb88cb461865deabc42471b3c2
Alaa abdelmajied <20alaa00@gmail.com>
2022-04-05 00:15:49 +0200

    adding appointment schema & editing patient schema

---
+++ models/Appointment.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const appointmentSchema = new mongoose.Schema({
+    patient: {
+        type: mongoose.Types.ObjectId
+    },
+    doctor: {
+        type: mongoose.Types.ObjectId
+    },
+    hopsital: {
+        type: mongoose.Types.ObjectId
+    },
+    date: {
+        type: Date
+    },
+    report: {
+        type: String
+    },
+    prescription: {
+        type: String
+    }
+})
+
+const Appointment = mongoose.model('appointment', patientSchema);
+
+module.exports = Appointment;
--- models/Patient.js
+++ models/Patient.js
@@ -16,16 +16,20 @@ const patientSchema = new mongoose.Schema({
         minlength: [6, 'Minimum password length is 6 characters']
     },
     name: {
-        type: String
+        type: String,
+        required: [true, 'Please enter a name'],
     },
     age: {
-        type: Number
+        type: Number,
+        required: [true, 'Please enter an age'],
     },
     phoneNumber: {
-        type: Number
+        type: Number,
+        required: [true, 'Please enter a phone number'],
     },
     dateOfBirth: {
-        type: Date
+        type: Date,
+        required: [true, 'Please enter a date of birth'],
     },
     avatar: {
         type: Buffer
@@ -36,6 +40,13 @@ const patientSchema = new mongoose.Schema({
     },
     questions: {
         type: Boolean
+    },
+    appointments:{
+        type: [mongoose.Types.ObjectId]
+    },
+    verified: {
+        type: Boolean,
+        default: false
     }
 });
 
@@ -62,6 +73,6 @@ patientSchema.statics.hashPassword = async function (password) {
     return bcrypt.hash(this.password, salt);
 }
 
-const Patient = mongoose.model('patient',patientSchema);
+const Patient = mongoose.model('patient', patientSchema);
 
 module.exports = Patient;
