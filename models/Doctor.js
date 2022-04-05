7daa5650367cbda1b728a508b6c603c9c126a2f6
nadeenelgazar <nadeen.khaled.2008@gmail.com>
2022-04-05 03:54:18 +0200

    doctor schema is added

---
+++ models/Doctor.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const { isEmail } = require('validator');
+
+
+const scheduleSchema =new mongoose.Schema({
+    day:
+    {
+        type:String,
+        enum:['monday','tuesday','wednesday','thursday','friday','saturday','sunday'],
+        lowercase:true
+    },     
+    from:String ,
+    to:String});
+
+const doctorSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,'name is required']
+    },
+    specialization:{
+        type:String,
+        required:[true,'specialization is required']
+    },
+    email: {
+        type: String,
+        required: [true, 'Please enter an email'],
+        unique: true,
+        lowercase: true,
+        validate: [isEmail, 'Please enter a valid email']
+    },
+    schedule:{
+        type:[scheduleSchema],
+        required:true,
+        lowercase:true
+    },
+    password: {
+        type: String,
+        required: [true, 'Please enter a password'],
+        minlength: [6, 'Minimum password length is 6 characters']
+    },
+})
