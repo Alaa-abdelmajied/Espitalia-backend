a0023b9159ae02e48207422f2f6ed1c31747d86c
Maram Ghazal <Maram Ghazal>
2022-04-05 05:53:29 +0200

    receptionist schema added

---
+++ models/ReceptionistSchema.js
@@ -0,0 +1,75 @@
+const mongoose = require('mongoose');
+const { isEmail } = require('validator');
+
+const receptionistSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please enter a receptionist name']
+    },
+    username: {
+        type: String,
+        required: [true]
+    },
+    email: {
+        type: String,
+        required: [true, 'Please enter an email'],
+        unique: true,
+        lowercase: true,
+        validate: [isEmail, 'Please enter a valid email']
+    },
+    password: {
+        type: String,
+        required: [true, 'Please enter a password'],
+        minlength: [6, 'Minimum password length is 6 characters']
+    },
+    hospital_name: {
+        type: String,
+        required: [true, 'Please enter a hospital name']
+    },
+    phone_number: {
+        type: Number,       // better as a string or number?
+        required: [true, 'Please enter a valid phone number'],
+        minlength: [11, 'Minimum phone number length is 11']
+    },
+    education: {
+        type: String
+    },
+    from: {
+        type: String
+    },
+
+    /***** days & time -> later *****/
+
+    /*working_hours: {
+        type: [Number], 
+        required: [true, 'Please enter working hours']
+    },
+    time:{
+        type:[String]
+    }*/
+
+});
+
+const Receptionist = mongoose.model('receptionist', receptionistSchema);
+
+//test
+async function createReceptionist() {
+    const user = new Receptionist({
+        name: 'Maram',
+        username: 'maram98',
+        email: 'maram_98@gmail.com',
+        password: 'hiiiiiii',
+        hospital_name: 'Al Andalusia',
+        phone_number: 12345678910,
+        education: 'Faculty of Commerce',
+        from: 'Alexandria, Egypt'
+
+    });
+
+    const result = await user.save();
+    console.log(result);
+}
+
+createReceptionist();
+
+module.exports = Receptionist;
