272241a7ad60bacd44ca3b1a500a760e6d6bf514
Mayar Adel <48599480+Mayaradel@users.noreply.github.com>
2022-04-27 15:53:05 +0200

    notifi.s

--- app.js
+++ app.js
@@ -6,12 +6,13 @@ const patientRouters = require('./routes/PatientRoutes')
 const app = express();
 app.use(express.json());
 
-const dbURI = 'mongodb+srv://Alaa:FpX3KihZBF5jaCV@espitaliacluster.ozn3j.mongodb.net/espitaliaDb?retryWrites=true&w=majority';
+//const dbURI = 'mongodb+srv://Alaa:FpX3KihZBF5jaCV@espitaliacluster.ozn3j.mongodb.net/espitaliaDb?retryWrites=true&w=majority';
+const dbURI='mongodb+srv://mayaradel:mayaradel1010@cluster0.m0yfu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then((result) => {
-        console.log('Server Connected!');
-        app.listen(3000);
-    })
-    .catch((err) => console.log(err));
+.then((result) => {
+    console.log('Server Connected!');
+    app.listen(3000);
+})
+.catch((err) => console.log(err));
 
 app.use('/patient', patientRouters);
