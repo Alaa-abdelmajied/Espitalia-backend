ea2293052f6fa90c45b7b764ee9690ce845e3248
OmarShalaby24 <ramo_24@outlook.com>
2022-04-27 02:58:50 +0200

    pull from master

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -1,35 +1,283 @@
 const Patient = require('../models/Patient');
+const Doctor = require('../models/Doctor');
+const Hospital = require('../models/Hospital');
+const Appointment = require('../models/Appointment');
+const WaitingVerfication = require('../models/WaitingVerfication');
 
-module.exports.patientLogin = async (req, res) => {
-    const { email, password } = req.body;
+const nodemailer = require('nodemailer');
+const otpGenerator = require('otp-generator');
+const jwt = require('jsonwebtoken');
+
+// const maxAge = 3 * 24 * 60 * 60;
+const createToken = (id) => {
+    return jwt.sign({ id }, 'Grad_Proj.Espitalia#SecRet.Application@30132825275'/*, {
+        expiresIn: maxAge
+    }*/);
+};
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'espitalia.app.gp',
+        pass: 'Espitalia@app.com'
+    }
+});
+
+const sendOtp = async (patientId, patientName, email) => {
+    const otp = otpGenerator.generate(5, {
+        digits: true, upperCaseAlphabets: false,
+        lowerCaseAlphabets: false, specialChars: false
+    });
+    await WaitingVerfication.create({
+        patient: patientId,
+        otp: otp
+    });
+    const confirmationMail = {
+        from: 'espitalia.app.gp@gmail.com',
+        to: email,
+        subject: 'Verify your account',
+        html: 'Dear ' + patientName + ',<br/><br/>Welcome to Espitalia.<br/><br/> Please enter this code in the application: <br/>' + otp + '<br/><br/>Thanks and regards , <br/>      Espitalia'
+    }
+    transporter.sendMail(confirmationMail, function (error, info) {
+        if (error) {
+            console.log("Email" + error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+}
+
+module.exports.patientSignup = async (req, res) => {
+    const { email, password, name, phoneNumber,
+        dateOfBirth, questions } = req.body;
     try {
-        const patient = await Patient.patientLogin(email,password);
-        
+        const patient = await Patient.create({
+            email, password, name, phoneNumber,
+            dateOfBirth, questions
+        });
+        // const otp = otpGenerator.generate(5, {
+        //     digits: true, upperCaseAlphabets: false,
+        //     lowerCaseAlphabets: false, specialChars: false
+        // });
+        // const patientId = patient.id;
+        const token = createToken(patient.id);
+        sendOtp(patient.id, patient.name, patient.email);
+        // const waitingVerfication = 
+        // await WaitingVerfication.create({
+        //     patient: patientId,
+        //     otp: otp
+        // });
+        // const confirmationMail = {
+        //     from: 'espitalia.app.gp@gmail.com',
+        //     to: email,
+        //     subject: 'Verify your account',
+        //     html: 'Dear ' + name + ',<br/><br/>Welcome to Espitalia.<br/><br/> Please enter this code in the application: <br/>' + otp + '<br/><br/>Thanks and regards , <br/>      Espitalia'
+        // }
+        // transporter.sendMail(confirmationMail, function (error, info) {
+        //     if (error) {
+        //         console.log("Email" + error);
+        //     } else {
+        //         console.log('Email sent: ' + info.response);
+        //     }
+        // });
+        res.status(201).send(token);
     } catch (err) {
         res.status(400).send(err.message);
     }
 }
 
-// module.exports.getPatient = async(req, res) => {
-//     const { name } = req.body;
-//     const patient = await Patient.find({name:name}).select('name');
-//     if (!patient) return res.status(404).send("No patients.");
-//     res.send(patient);
-//     console.log(patient);
-// }
-
-module.exports.getPatient=async(req,res) => {
-    //const {name} = req.body;
-    //const patient=await Patient.find();
-    const patient=await Patient.find();
-    console.log(patient);
-    try{
-
-        res.send(patient);
-        // const patients= await Patient.find(id);
-
+module.exports.patientLogin = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const patient = await Patient.patientLogin(email, password);
+        const token = createToken(patient.id);
+        if (!patient.verified) {
+            sendOtp(patient.id, patient.name);
+            res.status(200).send({ verified: patient.verified, token })
+        } else {
+            res.status(200).send({ verified: patient.verified, token })
+        }
+        // res.status(200).send(token);
+    } catch (err) {
+        res.status(400).send(err.message);
     }
-    catch (err) {
+}
+
+module.exports.verifyAccount = async (req, res) => {
+    const { otp, token, forgot } = req.body;
+    try {
+        const decodedToken = jwt.verify(token, 'Grad_Proj.Espitalia#SecRet.Application@30132825275');
+        const waitingVerfication = await WaitingVerfication.findOne(decodedToken);
+        if (otp == waitingVerfication.otp) {
+            if (!forgot) {
+                const patient = await Patient.findOne(waitingVerfication.account);
+                await Patient.updateOne(patient, {
+                    verified: true
+                });
+                await WaitingVerfication.deleteOne(decodedToken);
+            }
+            res.status(200).send('Verified');
+        } else {
+            res.status(400).send('Wrong Otp');
+        }
+    } catch (err) {
+        res.status(400).send(err.message);
+    }
+}
+
+module.exports.patientChangePassword = async (req, res) => {
+    const { oldPassword, newPassword, token } = req.body;
+    try {
+        const decodedToken = jwt.verify(token, 'Grad_Proj.Espitalia#SecRet.Application@30132825275');
+        // const patient = await Patient.findOne(decodedToken);
+        const result = await Patient.changePassword(decodedToken, oldPassword, newPassword);
+        res.status(200).send(result);
+    } catch (err) {
+        res.status(400).send(err.message);
+    }
+}
+
+module.exports.patientForgotPassword = async (req, res) => {
+    const { email } = req.body;
+    try {
+        const patient = await Patient.findOne({ email });
+        if (patient) {
+            sendOtp(patient.id, patient.name);
+            const token = createToken(patient.id);
+            res.status(201).send(token);
+        } else {
+            res.status(404).send('This email does not exist');
+        }
+    } catch (err) {
+        res.status(400).send(err.message);
+    }
+}
+
+module.exports.patientForgotPasswordChange = async (req, res) => {
+    const { newPassword, token } = req.body;
+    try {
+        const decodedToken = jwt.verify(token, 'Grad_Proj.Espitalia#SecRet.Application@30132825275');
+        const result = await Patient.forgotPassword(decodedToken, newPassword);
+        res.status(200).send(result);
+    } catch (err) {
+        res.status(400).send(err.message);
+    }
+}
+
+module.exports.patientLogin = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const patient = await Patient.patientLogin(email, password);
+
+    } catch (err) {
+        res.status(400).send(err.message);
+    }
+}
+
+//search be asma2 el drs bas
+module.exports.patientSearchDoctor = async (req, res) => {
+    const { search } = req.body;
+    try {
+        res.send(await Doctor.find({ name: { $regex: ".*" + search + ".*" } }));
+
+    } catch {
+        res.status(400).send(err.message);
+    }
+}
+
+//search be Specialization bas
+module.exports.patientSearchSpecialization = async (req, res) => {
+    const { search } = req.body;
+    try {
+
+        res.send(await Doctor.find({ specialization: { $regex: ".*" + search + ".*" } }));
+
+    } catch {
+        res.status(400).send(err.message);
+    }
+}
+
+//search be hospital bas
+module.exports.patientSearchHospital = async (req, res) => {
+    const { search } = req.body;
+    try {
+
+        res.send(await Hospital.find({ Name: { $regex: ".*" + search + ".*" } }));
+
+    } catch {
+        res.status(400).send(err.message);
+    }
+}
+
+//search be el talata (array w ba push fyha beltartyb 0:drs 1:hospital 2:specialization)
+module.exports.patientGeneralSerach = async (req, res) => {
+    const { search } = req.body;
+    try {
+
+        var result = new Array();
+        var doctors = await Doctor.find({ name: { $regex: ".*" + search + ".*" } });
+        var hospitals = await Hospital.find({ Name: { $regex: ".*" + search + ".*" } });
+        var specializations = await Doctor.find({ specialization: { $regex: ".*" + search + ".*" } });
+
+        result.push(doctors);
+        result.push(hospitals);
+        result.push(specializations);
+
+        // console.log(result);
+
+        res.send(result);
+
+    } catch {
+        res.status(400).send(err.message);
+    }
+}
+
+//function when pressed on specefic hospital it will return its Specialization
+module.exports.pressOnHospital = async (req, res) => {
+    const { id } = req.body;
+    try {
+        res.send((await Hospital.find({ _id: id }))[0].Specialization);
+
+    } catch {
+        res.status(400).send(err.message);
+    }
+}
+
+//return doctors in specefic hospital in specefic Specialization
+module.exports.pressOnHospitalThenSpecialization = async (req, res) => {
+    const { id, search } = req.body;
+    try {
+        res.send(await Doctor.find({ hospital_id: id, specialization: search }));
+
+    } catch {
+        res.status(400).send(err.message);
+    }
+
+}
+
+
+//Display Homepage 
+module.exports.displayHomepage = async (req, res) => {
+    try {
+        const doctor = await Doctor.aggregate([{ $project: { _id: 0, "name": 1, "specialization": 1, "rating": 1 } }, { '$sample': { 'size': 5 } }]);
+        const hospital = await Hospital.aggregate([{ $project: { _id: 0, "Name": 1, "Address": 1 } }, { '$sample': { 'size': 5 } }]);
+        const homepage_data = [doctor, hospital];
+        res.status(200).send(homepage_data);
+    } catch (err) {
+        res.status(400).send(err.message);
+    }
+}
+
+//Get Report
+module.exports.selectReport = async (req, res) => {
+    const { appointmentID } = req.body;
+    try {
+        const appointment = await Appointment.findOne({ _id: appointmentID });
+        const doctor = await Doctor.findOne({ _id: appointment.doctor });
+        const hospital = await Hospital.findOne({ _id: appointment.hospital });
+        const appointment_details = [{ dname: doctor.name }, { specialization: doctor.specialization }, { hname: hospital.Name }, { date: appointment.date }, { report: appointment.report }, { prescription: appointment.prescription }];
+        res.status(200).send(appointment_details);
+    } catch (err) {
         res.status(400).send(err.message);
     }
 }
