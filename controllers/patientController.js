5deec541d84c9a66a3c7ad782c61768cafef2f05
Maram Ghazal <Maram Ghazal>
2022-05-10 02:01:23 +0200

    editing patient controller

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -88,24 +88,21 @@ module.exports.patientSignup = async (req, res) => {
 };
 
 module.exports.patientLogin = async (req, res) => {
-    const { email, password } = req.body;
-    try {
-        const patient = await Patient.patientLogin(email, password);
-        const token = createToken(patient.id);
-        if (!patient.verified) {
-            sendOtp(patient.id, patient.name, patient.email);
-            res.status(200).send({ verified: patient.verified, token })
-        } else {
-            await Patient.updateOne(patient, {
-                loggedIn: true
-            });
-            res.status(200).send({ verified: patient.verified, token })
-        }
-    } catch (err) {
-        res.status(404).send(err.message);
+  const { email, password } = req.body;
+  try {
+    const patient = await Patient.patientLogin(email, password);
+    const token = createToken(patient.id);
+    if (!patient.verified) {
+      sendOtp(patient.id, patient.name, patient.email);
+      res.status(200).send({ verified: patient.verified, token });
+    } else {
+      await Patient.updateOne(patient, {
+        loggedIn: true,
+      });
+      res.status(200).send({ verified: patient.verified, token });
     }
   } catch (err) {
-    res.status(400).send(err.message);
+    res.status(404).send(err.message);
   }
 };
 
@@ -716,15 +713,16 @@ module.exports.getFlowOfEntrance = async (req, res) => {
     res.status(400).send(err.message);
   }
 };
-  
-module.exports.getFlowOfEntrance = async (req, res) => {
-    const { id } = req.params;
-    try {
-        const { currentFlowNumber } = await Doctor.findOne({ _id: id });
-        res.status(200).send({ currentFlowNumber });
-    } catch (err) {
-        res.status(400).send(err.message);
 
+module.exports.getFlowOfEntrance = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const { currentFlowNumber } = await Doctor.findOne({ _id: id });
+    res.status(200).send({ currentFlowNumber });
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+};
 //function to update db scheudles most probably it will be moved to dr controller
 //var intervalID = setInterval(myCallback, 5000);
 async function myCallback() {
@@ -746,7 +744,7 @@ async function myCallback() {
         newSchedule.push(schedule);
       }
     }
-    
+
     if (flag) {
       //last day in schudles
       let length = dr.schedule.length;
